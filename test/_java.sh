924ff1276d9d7519d36e903ab3f1c04b5d732597
Niranjan Godbole <niranjan8192@gmail.com>
2017-01-24 09:57:10 +0000

    updated java tests to print java version

--- test/_java.sh
+++ test/_java.sh
@@ -8,15 +8,23 @@ printf "\n\n"
 echo "update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java"
 update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
 printf "\n"
+java -version
+printf "\n"
 
 echo "update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+printf "\n"
+java -version
 printf "\n"
 
 echo "update-alternatives --set java /usr/lib/jvm/java-7-oracle/jre/bin/java"
 update-alternatives --set java /usr/lib/jvm/java-7-oracle/jre/bin/java
 printf "\n"
+java -version
+printf "\n"
 
 echo "update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java"
 update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
+printf "\n"
+java -version
 printf "\n"
