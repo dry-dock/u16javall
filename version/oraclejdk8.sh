bc3fdd17e80c3f8c261d3a2d13360716a82a1f7c
Bharath Upadhya <1992bharath@gmail.com>
2017-10-24 13:37:34 +0530

    installs oracle-java9 from webupd8team ppa

--- version/oraclejdk8.sh
+++ version/oraclejdk8.sh
@@ -8,5 +8,3 @@ apt-get install -y oracle-java8-installer
 update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-8-oracle/bin/javac
 update-alternatives --set javaws /usr/lib/jvm/java-8-oracle/jre/bin/javaws
-echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> $HOME/.bashrc
-echo 'export PATH=$PATH:/usr/lib/jvm/java-8-oracle/jre/bin' >> $HOME/.bashrc
