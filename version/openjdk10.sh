d71b0525a197e3da2ae4ecc16a2dc6b682411a29
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-16 14:34:57 +0530

    Update openjdk10.sh

--- version/openjdk10.sh
+++ version/openjdk10.sh
@@ -5,10 +5,5 @@ add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 apt-get install -y -q openjdk-10-jdk
 
-update-alternatives --set java /usr/lib/jvm/java-10-oracle/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-10-oracle/bin/javac
-update-alternatives --set javaws /usr/lib/jvm/java-10-oracle/jre/bin/javaws
-
-echo 'export JAVA_HOME=/usr/lib/jvm/java-10-oracle' >> /etc/drydock/.env
-echo 'export PATH=$PATH:/usr/lib/jvm/java-10-oracle/bin' >> /etc/drydock/.env
-
+update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
