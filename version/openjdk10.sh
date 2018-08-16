6a93eee55f9a7477a4c83d4538e2d3a49c9b3460
Ubuntu <ubuntu@ip-80-0-0-44.ec2.internal>
2018-08-16 09:58:30 +0000

    latest version for u16javall image

--- version/openjdk10.sh
+++ version/openjdk10.sh
@@ -5,5 +5,5 @@ add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 apt-get install -y -q openjdk-10-jdk
 
-update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
+#update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/jre/bin/java
+#update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
