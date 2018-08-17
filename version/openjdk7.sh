009031d67f00408eb93409bfd544f1b61d6ed6cd
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-17 16:07:42 +0530

    Update openjdk7.sh

--- version/openjdk7.sh
+++ version/openjdk7.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
 echo "================ Installing openjdk-7-jdk ================="
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
 apt-get install -y openjdk-7-jdk
 #icedtea-7-plugin not supported on ubuntu 16.04
